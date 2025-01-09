7edf5a2113cde8d3c35786f6f7387a0811a5ac85
Juno9170 <ryanzhuturtle@gmail.com>
2025-01-09 10:31:52 -0500

    feat(docs): add math package

--- documentation/source.config.ts
+++ documentation/source.config.ts
@@ -1,7 +1,11 @@
-import { defineDocs, defineConfig } from 'fumadocs-mdx/config';
-
-export const { docs, meta } = defineDocs({
-  dir: 'content/docs',
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
+import { defineConfig } from 'fumadocs-mdx/config';
+ 
+export default defineConfig({
+  mdxOptions: {
+    remarkPlugins: [remarkMath],
+    // Place it at first so that it won't be changed by syntax highlighter
+    rehypePlugins: (v) => [rehypeKatex, ...v],
+  },
 });
-
-export default defineConfig();
