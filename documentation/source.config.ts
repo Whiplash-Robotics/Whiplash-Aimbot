120c076ad4ca1e78e1f8a1f7958aeb318bff5196
Juno9170 <ryanzhuturtle@gmail.com>
2025-01-09 10:54:02 -0500

    bug(docs): fix math package

--- documentation/source.config.ts
+++ documentation/source.config.ts
@@ -1,7 +1,11 @@
+import { defineDocs, defineConfig } from 'fumadocs-mdx/config';
 import rehypeKatex from 'rehype-katex';
 import remarkMath from 'remark-math';
-import { defineConfig } from 'fumadocs-mdx/config';
- 
+
+export const { docs, meta } = defineDocs({
+  dir: 'content/docs',
+});
+
 export default defineConfig({
   mdxOptions: {
     remarkPlugins: [remarkMath],
